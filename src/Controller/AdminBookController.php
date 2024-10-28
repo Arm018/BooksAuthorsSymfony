4c22626023bcf17ae2ae79d32e2d14067cdbe74e
Arm018 <arm0182626@gmail.com>
2024-10-28 12:57:11 +0400

    Added the book crud from the admin side

--- src/Controller/AdminBookController.php
+++ src/Controller/AdminBookController.php
@@ -2,11 +2,89 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Form\BookType;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminBookController extends AbstractController
 {
-    
+    private $entityManager;
+    private $bookRepository;
+
+    public function __construct(EntityManagerInterface $entityManager, BookRepository $bookRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->bookRepository = $bookRepository;
+    }
+
+    /**
+     * @Route("/admin/books", name="app_admin_book", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $books = $this->bookRepository->findAll();
+
+        return $this->render('admin/book/index.html.twig', [
+            'books' => $books,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/books/create", name="app_admin_book_create", methods={"GET", "POST"})
+     */
+    public function create(Request $request): Response
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($book);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_book');
+        }
+
+        return $this->render('admin/book/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/books/{id}/edit", name="app_admin_book_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Book $book): Response
+    {
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_book');
+        }
+
+        return $this->render('admin/book/edit.html.twig', [
+            'form' => $form->createView(),
+            'book' => $book,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/books/{id}/delete", name="app_admin_book_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Book $book): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $book->getId(), $request->request->get('_token'))) {
+            $this->entityManager->remove($book);
+            $this->entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_book');
+    }
 }
---
+++ src/Form/BookType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('description', TextType::class)
+            ->add('publicationYear', IntegerType::class)
+            ->add('author', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => function (Author $author) {
+                    return $author->getFirstName() . ' ' . $author->getLastName();
+                },
+                'placeholder' => 'Select an author',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
